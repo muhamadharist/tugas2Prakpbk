1bf0125150ac1d3bd5120a25ed59b09f1b739e13
muhamadharist <muhamadharist2704@gmail.com>
2024-05-04 13:34:51 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# vue-project
+
+![Screenshot1](https://github.com/aldopratama045/Vue-project/assets/96534591/ca3ecaea-ee87-405d-a8ae-dd14a7a34bda)
+![Screenshot2](https://github.com/aldopratama045/Vue-project/assets/96534591/6c44d2a8-fdfd-48c5-9a13-430ee3f6d74b)
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- 1. Beberapa elemen HTML yang berbeda -->
+    <h1>Welcome!!</h1>
+
+    <!-- 2. Elemen input teks -->
+    <input v-model="inputText" placeholder="Masukkan teks di sini" />
+
+    <!-- 3. Penggunaan class bindings dan style bindings -->
+    <div :class="{ 'highlighted': isHighlighted }" :style="{ color: textColor }">
+      Teks ini akan berubah warna ketika klik ubah saya.
+    </div>
+
+    <!-- 4. Interaksi dinamis dengan event listener -->
+    <button @click="changeText">Ubah Teks</button>
+    <p>{{ dynamicText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: '',
+      isHighlighted: false,
+      textColor: 'red',
+      dynamicText: 'Teks ini akan berubah!',
+    };
+  },
+  methods: {
+    changeText() {
+      this.dynamicText = 'Teks berhasil diubah!';
+      this.isHighlighted = !this.isHighlighted;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.highlighted {
+  background-color: yellow;
+}
+</style>
